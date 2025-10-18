f9c47a7c8cb57bbfdfe7fa41018583f39890034d
sathiyadev <sathiyadevs16@gmail.com>
2025-10-18 08:15:03 +0530

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,56 @@
+#to find the max and min element of an array
+class minmax():
+    def maxmin(self,arr):
+        max_val=arr[0]
+        min_val=arr[0]
+        for num in arr:
+            if num>max_val:
+                max_val=num
+            elif num<min_val:
+                min_val=num
+        return[min_val,max_val]
+sol=minmax()
+arr=[23,4,45,6454,42]
+res=sol.maxmin(arr)
+print(res)
+
+#to find the second largest
+class secondlarge():
+    def smax(self,arr):
+        large=arr[0]
+        seclarge=arr[0]
+        for num in arr:
+            if num>large:
+                seclarge=large
+                large=num
+            elif num>seclarge:
+                seclarge=num
+        return[large,seclarge]
+sel=secondlarge()
+arr=[73,4,5,646531554]
+ress=sel.smax(arr)
+print(ress)
+
+#finding the third largest element on O(n)
+class thirdlargest():
+    def thired(self,arr):
+        if len(arr)<3:
+            return -1
+        first=second=third=float('-inf')
+        for num in arr:
+            if num>first:
+                third=second
+                second=first
+                first=num
+            elif num>second and num!=first:
+                third=second
+                second=num
+            elif num>third and num!=first and num!=second:
+                third=num
+        return third
+
+        
+th=thirdlargest()
+arr=[2,32,343,434]
+reee=th.thired(arr)
+print(reee)
