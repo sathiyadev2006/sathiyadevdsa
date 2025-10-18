0adf32892a98b4ad6f6f5edd602a88d93ccee649
sathiyadev2006 <sathiyadevs16@gmail.com>
2025-10-18 19:33:18 +0530

    changed

--- minmax.py
+++ minmax.py
@@ -56,7 +56,7 @@ reee=th.thired(arr)
 print(reee)
 
 # find the missing number in an array (and i am using set)
-class missingelement:
+class missingelement():
     def miss(self,arru):
         s=set(arru)
         m=min(arru)
@@ -83,3 +83,53 @@ rept=travers()
 ar=[12,34,45,654]
 ans=rept.trav(ar,12)
 print(ans)
+
+#dutch national algorithm which is important 
+class dutch():
+    def national(self,a):
+        low=0
+        mid=0
+        high=len(a)-1
+        if a[mid]==0:
+            a[low],a[mid]=a[mid],a[low]
+            low+=1
+            mid+=1
+        elif a[mid]==1:
+            mid+=1
+        else a[mid]==2:
+            a[mid],a[high]=a[high],a[mid]
+            high-=1
+            return a
+
+reps=dutch()
+a=[0,1,2,0,2,1]
+ans=rept.trav(a)
+print(ans)
+
+#rotation
+class rotation():
+    def rotate(self,array):
+        l=len(array)
+        temp=array[-1]
+        for i in range (l-1,0,-1):
+            array[i]=array[i-1]
+        array[0]=temp
+        return array
+    def antirotate(self,array):
+        l=len(arr)
+        temp=array[0]
+        for i in range(l-1):
+            array[i]=array[i+1]
+        array[l-1]=temp
+ro=rotation()
+rol=ro.rotate(array=[1,4,2,5])
+rols=ro.antirotate(array=[1,4,2,5])
+print(rol)
+print(rols)
+
+#rotation and first element in last
+
+
+
+        
+
